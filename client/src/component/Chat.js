67b49ba8735a7c17d1599469a77b6d5779d182ce
Emmanuel Etukudo <emzzyyoung@gmail.com>
2021-03-24 22:19:25 +0100

    created basic setup for chat component

--- client/src/component/Chat.js
+++ client/src/component/Chat.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
 
+let socket;
 
-const Chat = ()=>{
+const Chat = ({location})=>{
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
+
+    const API = 'localhost:5000';
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search);
+
+        socket = io(API);
+
+        setName(name);
+        setRoom(room);
+
+        console.log(socket);
+    })
+   
     return(
         <div>
             <h1>Chat</h1>
--- client/src/component/Join.js
+++ client/src/component/Join.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
 
 
 const Chart = ()=>{
+    const [name, setName] = useState("");
+    const [room, setRoom] = useState("");
     return(
-        <div>
-            <h1>Join</h1>
+        <div className="joinOuterContainer">
+           <div className="joinInnerContainer">
+               <h1 className="heading">Join</h1>
+               <div><input placeholder="Name" className="joinInput" type="text" onChange={(e) => setName(e.target.value)} /></div>
+               <div><input placeholder="Room" className="joinInput" type="text" onChange={(e) => setRoom(e.target.value)} /></div>
+               <Link to={`/chat?name=${name}&room=${room}`}>
+               <button className="button" type="submit"> Sign In</button>
+               </Link>
+           </div>
         </div>
     )
 }
